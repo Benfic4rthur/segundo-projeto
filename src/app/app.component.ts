456cd6b289eb867a9994d28d37757dd560e29a9f
benfic4rthur <arthur_benfica@hotmail.com>
2024-07-02 16:16:21 -0300

    Aprofundando em binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
 
+interface IInput {
+  infos: IInfos;
+}
+
+interface IInfos {
+  type: string;
+  placeholder: string;
+  value: string;
+  readonly: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'segundo-projeto';
+  infos: IInfos = {
+    type: 'text',
+    value: '',
+    placeholder: 'Habilite para digitar',
+    readonly: true
+  };
+
+  habilitou() {
+    this.infos.readonly = false;
+  }
+
+  desabilitou() {
+    this.infos.readonly = true;
+  }
+
+  alertInputText() {
+    const userConfirmed = confirm(`O texto digitado foi "${this.infos.value}" \n \n deseja excluir?`);
+    if (userConfirmed) {
+      this.infos.value = '';
+    }
+  }
+
+  atualizarValor(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.infos.value = inputElement.value;
+  }
+
+  limparInput() {
+    const userConfirmed = confirm(`Deseja limpar o campo?`);
+    if (userConfirmed) {
+      this.infos.value = '';
+    }
+  }
 }
